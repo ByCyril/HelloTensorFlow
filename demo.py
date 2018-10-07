0723ff42f2f8246a8a70d3a0372f1616623e697c
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2018-10-06 19:49:35 -0700

    minor changes

--- demo.py
+++ demo.py
@@ -1,31 +1,33 @@
 
+
 import tensorflow as tf
 from tensorflow import keras
+import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 model = keras.Sequential()
+sess = tf.Session()
 
 input_layer = keras.layers.Dense(3, input_shape=[3], activation='tanh')
-
 model.add(input_layer)
 
 output_layer = keras.layers.Dense(1, activation='sigmoid')
-
 model.add(output_layer)
 
 gd = tf.train.GradientDescentOptimizer(0.01)
 
 model.compile(optimizer=gd, loss='mse')
 
-x = tf.Variable([[1,1,0],[1,1,1],[0,1,0],[-1,1,0],[-1,0,0],[-1,0,1],[0,0,1],[1,1,0],[1,0,0],[-1,0,0],[1,0,1],[0,1,1],[0,0,0],[-1,1,1]])
-y = tf.Variable([[0],[0],[1],[1],[1],[0],[1],[0],[1],[1],[1],[1],[1],[0]])
+training_x = tf.Variable([[1, 1, 0], [1, 1, 1], [0, 1, 0], [-1, 1, 0], [-1, 0, 0], [-1, 0, 1],[0, 0, 1], [1, 1, 0], [1, 0, 0], [-1, 0, 0], [1, 0, 1], [0, 1, 1], [0, 0, 0], [-1, 1, 1]])
+training_y = tf.Variable([[0], [0], [1], [1], [1], [0], [1],[0], [1], [1], [1], [1], [1], [0]])
 
-model.fit(x, y, epochs=5000, steps_per_epoch=10)
-model.save_weights('demo_model.h5')
+model.fit(training_x, training_y, epochs=1000, steps_per_epoch=10)
+# model.save_weights('demo_model.h5')
+# model.load_weights('demo_model.h5')
 
-results = model.predict(x, verbose=0, steps=1)
+text_x = tf.Variable([[1, 0, 0]])
+test_y = model.predict(text_x, verbose=0, steps=1)
 
-test = tf.Variable([[1,0,0]])
-test = model.predict(test, verbose=0, steps=1)
 
-print(results)
-print(test)
+print(test_y)
